ff7928209ca9bec5edbaa0d43dc358852b1a202b
Carlos Barreto <66956177+Carlosbarreto-info@users.noreply.github.com>
2023-12-08 14:01:48 +0000

    Add files via upload

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -16,7 +16,7 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(1, self.calc.add(1, 0))
 
     def test_add_method_returns_correct_result1(self):
-        self.assertEqual(6, self.calc.add(3, 3))
+        self.assertEqual(5, self.calc.add(3, 2))
         self.assertEqual(0, self.calc.add(2, -2))
         self.assertEqual(0, self.calc.add(-2, 2))
         self.assertEqual(1, self.calc.add(1, 0))      
@@ -39,6 +39,12 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
+
+    def test_divide_method_fails_with_division_by_zero(self):
+        self.assertRaises(TypeError, self.calc.divide, 2, 0)
+        self.assertRaises(TypeError, self.calc.divide, 2, -0)
+        self.assertRaises(TypeError, self.calc.divide, 0, 0)
+        self.assertRaises(TypeError, self.calc.divide, "0", 0)
 
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
